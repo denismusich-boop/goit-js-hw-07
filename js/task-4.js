d101c855492257cfe72bb477a8e76b001eb0df47
Denis Musich <denis.musich@gmail.com>
2026-05-30 14:18:23 +0300

    Implement form submission handling in task-4.js with validation and reset functionality

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,21 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", event => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget.elements;
+
+  const formData = {
+    email: email.value.trim(),
+    password: password.value.trim(),
+  };
+
+  if (formData.email === "" || formData.password === "") {
+    alert("All form fields must be filled in");
+    return;
+  }
+
+  console.log(formData);
+
+  event.currentTarget.reset();
+});
