44b86ee8060bf6c515904c9fe47646842fe7a1fb
Denis Musich <denis.musich@gmail.com>
2026-05-30 14:11:29 +0300

    Refactor input event handling in task-3.js to streamline name

--- js/task-3.js
+++ js/task-3.js
@@ -2,8 +2,7 @@ const input = document.querySelector("#name-input");
 const output = document.querySelector("#name-output");
 
 input.addEventListener("input", event => {
-  const name = event.target.value.trim();
-
-  output.textContent = name || "Anonymous";
+  console.log(event.target.value);
+  output.textContent = event.target.value.trim() || "Anonymous";
 });
 
